41cac0a36dbc77e1b00623684265f70b1f880adc
dktk55cfn5-wq <dktk55cfn5@privaterelay.appleid.com>
2026-03-12 10:39:33 -0700

    image

--- TVPoke/Pokemon/Magneton.py
+++ TVPoke/Pokemon/Magneton.py
@@ -4,9 +4,9 @@ from TVPoke.BaseClasses.Move import Move
 class Magneton(Electric):
     def __init__(self):
         moves = [
-            Move("SonicBoom", "NORMAL", 50),
+            Move("SonicBoom", "NORMAL", 100),
             Move("Slash", "NORMAL", 80),
             Move("Thunder Shock", "Electric", 50),
             Move("Thunder", "Electric", 120)
         ]  
-        super().__init__("Magneton", 110000, moves, "./TVPoke/Pokemon/imgs/Magneton.png")
+        super().__init__("Magneton", 310, moves, "./TVPoke/Pokemon/imgs/Magneton.png")
